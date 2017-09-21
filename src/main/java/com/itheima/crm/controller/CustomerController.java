a3d67ac8135c85db8550bee0d4fa67f7a96eb8d7
allan5945 <allan5945@163.com>
2017-09-21 10:34:47 +0800

    新增上传图片功能 - 修改

--- src/main/java/com/itheima/crm/controller/CustomerController.java
+++ src/main/java/com/itheima/crm/controller/CustomerController.java
@@ -9,16 +9,12 @@ import java.util.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.alibaba.fastjson.JSONObject;
 import com.itheima.crm.pojo.UserTableExample;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.itheima.crm.service.UserLoginService;
